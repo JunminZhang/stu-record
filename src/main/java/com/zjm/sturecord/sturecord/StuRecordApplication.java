f5b2e03ec54ed9e153649183afd6f53cf98c842f
ZJmGit <780792080@qq.com>
2019-01-21 22:04:44 +0800

    首次上传

---
+++ src/main/java/com/zjm/sturecord/sturecord/StuRecordApplication.java
@@ -0,0 +1,14 @@
+package com.zjm.sturecord.sturecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuRecordApplication.class, args);
+	}
+
+}
+
