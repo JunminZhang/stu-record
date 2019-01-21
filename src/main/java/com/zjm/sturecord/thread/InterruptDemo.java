ea4dedfff01647febac7f13806190969a3406a59
ZJmGit <780792080@qq.com>
2019-01-21 22:17:35 +0800

    新增中断线程方式

---
+++ src/main/java/com/zjm/sturecord/thread/InterruptDemo.java
@@ -0,0 +1,26 @@
+package com.zjm.sturecord.thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ *    优雅终断线程的方式
+ */
+public class InterruptDemo {
+
+    private static int i;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread=new Thread(()->{
+            while(!Thread.currentThread().isInterrupted()){
+                i++;
+            }
+            System.out.println(i);
+        },"interruptDemo");
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread.interrupt(); //设置interrupt标识为true
+        System.out.println(thread.isInterrupted());
+    }
+
+
+}
